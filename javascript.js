befda862736da4f50ecdbc2931b7ec5f70fe96d0
pariwisatabali <98495108+pariwisatabali@users.noreply.github.com>
2022-01-27 06:17:29 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+//check
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+        //display:block
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
+//scroll 
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 0) {
+            $("nav").addClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+        } else {
+            $("nav").removeClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+        }
+    })
+});
